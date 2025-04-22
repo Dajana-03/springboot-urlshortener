5f12b9821038d89e23ecdd8e11198d3b7083541c
Dajana-03 <pirjasidajana3@gmail.com>
2025-04-22 20:01:41 +0200

    Initial commit

---
+++ test/java/com/dajanapirjasi/UrlShortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dajanapirjasi.UrlShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
